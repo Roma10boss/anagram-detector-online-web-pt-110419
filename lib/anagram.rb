3f84f0a0024daf12472a4f7f2f48db30dda1b5be
Romario Gustave <romagustave10@gmail.com>
2019-12-31 18:03:43 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,4 +1,16 @@
 # Your code goes here!
 class Anagram 
   attr_accessor 
+  
+  def anagram_naive?(word1,word2)
+    array_one = word1.split
+    array_two = word2.split
+    array_one = array_one.sort 
+    array_two = array_two.sort 
+    if array_one == array_two
+      return true 
+    else 
+      return false 
+    end 
+    
 end 
